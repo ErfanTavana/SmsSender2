a79f67cb9ddf20bb5c715ad603b426b214268c2b
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-10-22 13:06:30 -0700

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.views import View
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            # return redirect()
+            pass
+    def post(self, request):
+        pass
