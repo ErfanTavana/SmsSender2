8d574d8a52e7b16be4c8cff96ca6fcf6920c149c
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-10-23 09:50:28 -0700

    Fix login bug

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,19 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import User  # مدل کاربر شما
+from .models import User  # Your custom User model
 
-class LoginSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, label="رمز عبور")
-    token = serializers.CharField(read_only=True, label="توکن")
-    user_id = serializers.IntegerField(read_only=True, label="شناسه کاربر")
-    phone_number_display = serializers.CharField(read_only=True, label="نمایش شماره تلفن")
-    first_name = serializers.CharField(read_only=True, label="نام")
-    last_name = serializers.CharField(read_only=True, label="نام خانوادگی")
-
-    class Meta:
-        model = User
-        fields = ['phone_number', 'password', 'token', 'user_id', 'phone_number_display', 'first_name', 'last_name']
+class LoginSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(label="شماره تلفن", write_only=True)
+    password = serializers.CharField(label="رمز عبور", write_only=True)
+    token = serializers.CharField(label="توکن", read_only=True)
+    user_id = serializers.UUIDField(label="شناسه کاربر", read_only=True)
+    phone_number_display = serializers.CharField(label="نمایش شماره تلفن", read_only=True)
+    first_name = serializers.CharField(label="نام", read_only=True)
+    last_name = serializers.CharField(label="نام خانوادگی", read_only=True)
 
     def validate(self, data):
         phone_number = data.get('phone_number')
@@ -22,6 +19,7 @@ class LoginSerializer(serializers.ModelSerializer):
         if not phone_number or not password:
             raise serializers.ValidationError('شماره تلفن و رمز عبور هر دو مورد نیاز هستند.')
 
+        # Using the default authenticate method
         user = authenticate(phone_number=phone_number, password=password)
 
         if user is None:
@@ -32,9 +30,9 @@ class LoginSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        token, _ = Token.objects.get_or_create(user=user)
         return {
-            'token': token.key,
+            'Authorization': f'Token {token.key}',
             'user_id': user.id,
             'phone_number_display': user.phone_number,
             'first_name': user.first_name,
