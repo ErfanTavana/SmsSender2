a6ae8b210278943b4084dccdf6ac3d9f9c30dd55
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-10-23 14:25:26 -0700

    The display of the message is finished.

--- contacts/views.py
+++ contacts/views.py
@@ -6,6 +6,7 @@ from SmsSender2.utils import normalize_phone_number
 from contacts.models import Contact
 from organizations.models import Group
 from .utils import check_user_organization
+from text_messages.models import Message
 
 
 class ContactApiView(APIView):
@@ -23,6 +24,7 @@ class ContactApiView(APIView):
 
         serializer = GroupSerializer(groups, many=True)
         return Response(data={'message': 'گروه‌ها', 'data': serializer.data}, status=status.HTTP_200_OK)
+
     def post(self, request):
         # استفاده از تابع کمکی برای بررسی کاربر و سازمان
         error_response, user, organization_user = check_user_organization(request)
@@ -31,16 +33,32 @@ class ContactApiView(APIView):
 
         serializer = ContactSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save(created_by=user, organization=organization_user)
+            contact = serializer.save(created_by=user, organization=organization_user)
+
+            # دریافت گروه انتخابی از درخواست
+            selected_groups = serializer.validated_data.get('groups', [])
+            last_message = None
+
+            if selected_groups:
+                selected_group = selected_groups[0]
+                last_message = Message.objects.filter(
+                    organization=organization_user,
+                    group_id=selected_group.id,
+                    message_type='فردی',
+                ).order_by('-created_at').first()
+
             return Response({
                 'message': 'مخاطب با موفقیت ایجاد شد.',
-                'data': {'contact_info': serializer.data}
+                'data': {
+                    'contact_info': serializer.data,
+                    'last_message': last_message.text if last_message else None  # استفاده از ویژگی صحیح
+                }
             }, status=status.HTTP_201_CREATED)
 
         # پردازش خطاها برای استخراج اولین پیام خطا
         first_error_message = next(iter(serializer.errors.values()))[0]
 
         return Response({
-            'message': first_error_message,  # اولین پیام خطا به عنوان رشته در message
-            'data': serializer.errors  # نگهداری همه خطاها در data
+            'message': first_error_message,
+            'data': serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
