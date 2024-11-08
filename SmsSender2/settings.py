0062188bfb3bd3428c91c9a2aebef120b5bb9b6e
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-11-08 04:46:51 -0800

    uodate

--- SmsSender2/settings.py
+++ SmsSender2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +82,11 @@ WSGI_APPLICATION = 'SmsSender2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
