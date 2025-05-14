95e3812de67b5cefce88c0437c08f3e56df3ccd1
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2025-05-14 19:35:23 +0330

    Update admin.py

--- dastgah/admin.py
+++ dastgah/admin.py
@@ -1,21 +1,45 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Device, DeviceLog
+
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("uid", "name", "location", "description")
-    search_fields = ("uid", "name", "location")
-    list_filter = ("location",)
-    ordering = ("uid",)
+    list_display = ('uid', 'name', 'location', 'description')
+    search_fields = ('uid', 'name', 'location')
+    ordering = ('uid',)
+
 
 @admin.register(DeviceLog)
 class DeviceLogAdmin(admin.ModelAdmin):
-    list_display = ("timestamp", "device", "level", "message_short", "module", "code")
-    search_fields = ("message", "module", "device__uid", "device__name")
-    list_filter = ("level", "module", "timestamp")
-    ordering = ("-timestamp",)
-    readonly_fields = ("timestamp",)
+    list_display = ('colored_level', 'timestamp', 'device', 'short_message', 'module', 'code')
+    list_filter = ('level', 'device', 'module')
+    search_fields = ('message', 'module', 'device__uid')
+    ordering = ('-timestamp',)
+    readonly_fields = ('timestamp',)
+    list_per_page = 50
 
-    def message_short(self, obj):
-        return (obj.message[:60] + "...") if len(obj.message) > 60 else obj.message
-    message_short.short_description = "Message"
+    def short_message(self, obj):
+        return (obj.message[:75] + '...') if len(obj.message) > 75 else obj.message
+    short_message.short_description = "Message"
+
+    def colored_level(self, obj):
+        color_map = {
+            'DEBUG': 'gray',
+            'INFO': 'blue',
+            'WARNING': 'orange',
+            'ERROR': 'red',
+            'CRITICAL': 'darkred',
+        }
+        return format_html(
+            '<strong style="color:{}">{}</strong>',
+            color_map.get(obj.level, 'black'),
+            obj.level
+        )
+    colored_level.short_description = 'Level'
+
+    # نمایش زیبای JSON
+    def extra_data_pretty(self, obj):
+        import json
+        return format_html('<pre>{}</pre>', json.dumps(obj.extra_data, indent=2, ensure_ascii=False)) if obj.extra_data else "-"
+    extra_data_pretty.short_description = "Extra Data"
