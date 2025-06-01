7e6f4837ee1b6073a1bad8719585ca822af8fbaf
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2025-06-01 13:30:35 +0330

    update

--- dastgah/urls.py
+++ dastgah/urls.py
@@ -3,5 +3,5 @@ from .views import DeviceLogCreateAPIView , ReceiveSMSAPIView
 
 urlpatterns = [
     path('device_logs/', DeviceLogCreateAPIView.as_view(), name='device-log-create'),
-    path('api/receive-sms/', ReceiveSMSAPIView.as_view(), name='receive-sms'),
+    path('api/receive-sms/663489aa8de57a7c6534fbe0ad71b17be95fc82e/', ReceiveSMSAPIView.as_view(), name='receive-sms'),
 ]
